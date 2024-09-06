c9b2b0d30cdcc841dea296ef51ea21b75e812c8d
datnguyennnx <official.nguyendat@gmail.com>
2024-09-06 11:02:33 +0700

    chore: format and lint source code

--- recommender-ui/src/components/Dot.tsx
+++ recommender-ui/src/components/Dot.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react'
 
 interface DotProps {
-  status: boolean;
+    status: boolean
 }
 
 const Dot: React.FC<DotProps> = ({ status }) => {
-  const baseClasses = "inline-block w-2 h-2 rounded-full mr-2";
-  const colorClass = status ? "bg-green-500" : "bg-red-500";
+    const baseClasses = 'inline-block w-2 h-2 rounded-full mr-2'
+    const colorClass = status ? 'bg-green-500' : 'bg-red-500'
 
-  return <span className={`${baseClasses} ${colorClass} animate-pulse`}></span>;
-};
+    return (
+        <span className={`${baseClasses} ${colorClass} animate-pulse`}></span>
+    )
+}
 
-export default Dot;
+export default Dot
